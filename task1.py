cbbd3120d281663aca1a48fbe397c4f3a275d61e
elijahrussell484-droid <elijah.russell484@students.cmcss.net>
2025-12-18 08:10:21 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+slices = party_pizza_mini + large + medium
+print(f"Toatl number of slices: {slices}")
 
+people += 1
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+people += 2
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+
+
+slices += party_pizza_mini
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+print("...for Mr. Hollow Leg")
